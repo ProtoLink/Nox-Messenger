8cc91a80aee4453a2aaa385d925d915e94666522
ProtoLink <46989553+ProtoLink@users.noreply.github.com>
2025-06-21 21:19:51 -0400

    Update main.js

--- main.js
+++ main.js
@@ -34,7 +34,7 @@ wss.on("connection", function (ws, req) {
       console.log('keepAlive');
       return;
     }
-    broadcast(ws, stringifiedData, false);
+    broadcast(ws, stringifiedData, true);
   });
 
   ws.on("close", (data) => {
